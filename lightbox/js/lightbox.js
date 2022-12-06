7b84163f3bb459574f70e9e0832429ead563924a
MarcJoseph17 <115659452+MarcJoseph17@users.noreply.github.com>
2022-12-06 13:52:00 -0500

    updated

--- lightbox/js/lightbox.js
+++ lightbox/js/lightbox.js
@@ -13,25 +13,77 @@ function openLightbox(){
 }
 
 let coffeImage1 = document.getElementById ('coffee1');
-
-
 coffeeImage1.onclick = openLightbox;
-coffeeImage2.onclick = openLightbox;
-coffeeImage3.onclick = openLightbox;
-coffeeImage4.onclick = openLightbox;
-coffeeImage5.onclick = openLightbox;
-coffeeImage6.onclick = openLightbox;
-coffeeImage7.onclick = openLightbox;
-coffeeImage8.onclick = openLightbox;
-coffeeImage9.onclick = openLightbox;
-coffeeImage10.onclick = openLightbox;
-coffeeImage11.onclick = openLightbox;
-coffeeImage12.onclick = openLightbox;
-
 function closeLightbox() {
         lightboxContainer.classList.remove('display');
         lightbox.src = "";
 
+}
+let coffeImage2 = document.getElementById ('coffee2');
+coffeeImage2.onclick = openLightbox;
+function closeLightbox() {
+        lightboxContainer.classList.remove('display');
+        lightbox.src = "";
+}
+let coffeImage3 = document.getElementById ('coffee3');
+coffeeImage3.onclick = openLightbox;
+function closeLightbox() {
+        lightboxContainer.classList.remove('display');
+        lightbox.src = "";
+}
+let coffeImage4 = document.getElementById ('coffee4');
+coffeeImage4.onclick = openLightbox;
+function closeLightbox() {
+        lightboxContainer.classList.remove('display');
+        lightbox.src = "";
+}
+let coffeImage5 = document.getElementById ('coffee5');
+coffeeImage5.onclick = openLightbox;
+function closeLightbox() {
+        lightboxContainer.classList.remove('display');
+        lightbox.src = "";
+}
+let coffeImage6 = document.getElementById ('coffee6');
+coffeeImage6.onclick = openLightbox;
+function closeLightbox() {
+        lightboxContainer.classList.remove('display');
+        lightbox.src = "";
+}
+let coffeImage7 = document.getElementById ('coffee7');
+coffeeImage7.onclick = openLightbox;
+function closeLightbox() {
+        lightboxContainer.classList.remove('display');
+        lightbox.src = "";
+}
+let coffeImage8 = document.getElementById ('coffee8');
+coffeeImage8.onclick = openLightbox;
+function closeLightbox() {
+        lightboxContainer.classList.remove('display');
+        lightbox.src = "";
+}
+let coffeImage9 = document.getElementById ('coffee9');
+coffeeImage9.onclick = openLightbox;
+function closeLightbox() {
+        lightboxContainer.classList.remove('display');
+        lightbox.src = "";
+}
+let coffeImage10 = document.getElementById ('coffee10');
+coffeeImage10.onclick = openLightbox;
+function closeLightbox() {
+        lightboxContainer.classList.remove('display');
+        lightbox.src = "";
+}
+let coffeImage11 = document.getElementById ('coffee11');
+coffeeImage11.onclick = openLightbox;
+function closeLightbox() {
+        lightboxContainer.classList.remove('display');
+        lightbox.src = "";
+}
+let coffeImage12 = document.getElementById ('coffee12');
+coffeeImage12.onclick = openLightbox;
+function closeLightbox() {
+        lightboxContainer.classList.remove('display');
+        lightbox.src = "";
 }
 lightboxCloser.onclick = closeLightbox
 lightboxBackground.onclick = closeLightbox
