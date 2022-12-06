8e61f67508e3405f84cf6612407dbf5ac947e141
MarcJoseph17 <115659452+MarcJoseph17@users.noreply.github.com>
2022-12-06 13:40:39 -0500

    ....

--- lightbox/js/lightbox.js
+++ lightbox/js/lightbox.js
@@ -14,7 +14,19 @@ function openLightbox(){
 
 let coffeImage1 = document.getElementById ('coffee1');
 
+
 coffeeImage1.onclick = openLightbox;
+coffeeImage2.onclick = openLightbox;
+coffeeImage3.onclick = openLightbox;
+coffeeImage4.onclick = openLightbox;
+coffeeImage5.onclick = openLightbox;
+coffeeImage6.onclick = openLightbox;
+coffeeImage7.onclick = openLightbox;
+coffeeImage8.onclick = openLightbox;
+coffeeImage9.onclick = openLightbox;
+coffeeImage10.onclick = openLightbox;
+coffeeImage11.onclick = openLightbox;
+coffeeImage12.onclick = openLightbox;
 
 function closeLightbox() {
         lightboxContainer.classList.remove('display');
