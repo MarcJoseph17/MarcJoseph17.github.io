b87fe1178d6f63dd940f17b14660040f9db75e84
MarcJoseph17 <115659452+MarcJoseph17@users.noreply.github.com>
2022-12-12 11:09:58 -0500

    ..

--- GSAPanimation/js/gasap.js
+++ GSAPanimation/js/gasap.js
@@ -24,3 +24,25 @@ gsap.from(".headline", {
     y: 150
 
 })
+gsap.from(".first", {
+    duration: 2.5,
+    stagger: 1.5,
+    opacity: 0,
+    y: 150
+
+})
+
+gsap.from(".second", {
+    duration: 4.5,
+    stagger: 2.5,
+    opacity: 0,
+    y: 550
+
+})
+gsap.from(".third", {
+    duration: 5.5,
+    stagger: 3.5,
+    opacity: 0,
+    y: 350
+
+})
